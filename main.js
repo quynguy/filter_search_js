d80d1dfd72894d5a6808609c101e0ec77ee9cc67
wquynguy <wquyenle@gmail.com>
2024-05-16 10:47:54 -0500

    enhanced product card layout and contents

--- main.js
+++ main.js
@@ -32,16 +32,32 @@ let drugs = {
 };
 
 for (let i of drugs.data) {
+    //card
     let card = document.createElement("div");
-    card.classList.add("card","i.dosageForm");
-
+    card.classList.add("card", "i.dosageForm");
+    // image container
     let imgContainer = document.createElement("div");
     imgContainer.classList.add("image-container");
-    
+    // card image
     let image = document.createElement("img");
     image.setAttribute("src", i.image);
     imgContainer.appendChild(image);
     card.appendChild(imgContainer);
+    // product container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    // product name
+    let name = document.createElement("h4");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    // product price
+    let price = document.createElement("h6");
+    price.innerText = "$" +i.price;
+    container.appendChild(price);
 
+
+    card.appendChild(container);
     document.getElementById("products").appendChild(card);
+    
 }

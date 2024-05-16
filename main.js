c12e51cd17914a7a9fef5aa499b44bd4191fa7c1
wquynguy <wquyenle@gmail.com>
2024-05-16 10:22:46 -0500

    initialized product database with sample entries

---
+++ main.js
@@ -0,0 +1,32 @@
+let products = {
+    data: [
+        {
+            productName: "Abilify ",
+            dosageForm: "Tablets/Capsules",
+            price: "100",
+            quantity: "500",
+            image: "images/abilify.jpg",
+        },
+        {
+            productName: "Aimovig ",
+            dosageForm: "Injectables/Injections",
+            price: "1000",
+            quantity: "1",
+            image: "images/aimovig.jpg",
+        },
+        {
+            productName: "Amoxicillin Oral Powder",
+            dosageForm: "Syrups/Liquids",
+            price: "75",
+            quantity: "1",
+            image: "images/amoxicillinRecon.jpg",
+        },
+        {
+            productName: "Tobramycin Ophthalmic Solution",
+            dosageForm: "Drops",
+            price: "175",
+            quantity: "1",
+            image: "images/tobramycin.jpg",
+        },
+    ],
+};
